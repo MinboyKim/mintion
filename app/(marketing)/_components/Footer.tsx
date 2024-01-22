c6fb6df9361f32110cf000f43bd18cc82f5d6404
MinboyKim <kdm3801@gmail.com>
2024-01-22 23:30:24 +0900

    feat: dark mode
    다크모드 기능 추가

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -3,9 +3,15 @@ import { Logo } from "./Logo";
 
 export const Footer = () => {
     return (
-        <div className="flex items-center w-full p-6 bg-background z-50">
+        <div
+            className="flex items-center w-full p-6 bg-background
+         z-50 dark:bg-[#1f1f1f]"
+        >
             <Logo />
-            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+            <div
+                className="md:ml-auto w-full justify-between 
+            md:justify-end flex items-center gap-x-2 text-muted-foreground"
+            >
                 <Button variant="ghost" size="sm">
                     Private Policy
                 </Button>
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 
@@ -10,13 +11,13 @@ export const Navbar = () => {
     return (
         <div
             className={cn(
-                "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+                "z-50 bg-background fixed top-0 flex items-center w-full p-6 dark:bg-[#1f1f1f]",
                 scrolled && "border-b shadow-sm"
             )}
         >
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                Login
+                <ModeToggle />
             </div>
         </div>
     );
