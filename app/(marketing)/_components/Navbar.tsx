ca658fd02b3c073eb0ee74d5b460b4bc8e0ac9fd
MinboyKim <kdm3801@gmail.com>
2024-01-24 20:48:55 +0900

    feat: auth
    Convex와 Clerk로 Authentication 구현

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
 
     return (
@@ -17,6 +24,27 @@ export const Navbar = () => {
         >
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && <Spinner />}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">Get Mintion free</Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">Enter Mintion</Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://upward-pangolin-46.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ],
+};
