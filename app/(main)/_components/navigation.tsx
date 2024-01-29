fa4801eebca46cfeaba3e559ccc931394abb0271
MinboyKim <kdm3801@gmail.com>
2024-01-30 00:24:23 +0900

    fix: Expand bug fix
    문서 하나도 없을 시 사이드바 출력 메세지 버그 수정

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import {
     ChevronsLeft,
     MenuIcon,
+    Plus,
     PlusCircle,
     Search,
     Settings,
@@ -157,6 +158,11 @@ export const Navigation = () => {
                 </div>
                 <div className="mt-4">
                     <DocumentList />
+                    <Item
+                        onClick={handleCreate}
+                        icon={Plus}
+                        label="Add a page"
+                    />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
