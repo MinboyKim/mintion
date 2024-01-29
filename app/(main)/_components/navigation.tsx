059168884182cb97e7c22b04fa63dc143bcbfc31
MinboyKim <kdm3801@gmail.com>
2024-01-29 19:41:01 +0900

    feat: Create child note
    자식 노트 생성 기능 구현

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,15 +12,15 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
@@ -108,7 +108,7 @@ export const Navigation = () => {
     };
 
     const handleCreate = () => {
-        const promise = create({ title: "Untitle" });
+        const promise = create({ title: "Untitled" });
 
         toast.promise(promise, {
             loading: "Creating a new note...",
@@ -156,9 +156,7 @@ export const Navigation = () => {
                     />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document) => (
-                        <p key={document._id}>{document.title}</p>
-                    ))}
+                    <DocumentList />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
