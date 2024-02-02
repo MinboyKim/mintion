822329997f2bc8718076635cb2c04506e8e2efd4
MinboyKim <kdm3801@gmail.com>
2024-02-02 15:00:15 +0900

    feat: TrashBox
    휴지통 기능 구현

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -8,6 +8,7 @@ import {
     PlusCircle,
     Search,
     Settings,
+    Trash,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
@@ -18,6 +19,12 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -164,6 +171,17 @@ export const Navigation = () => {
                         label="Add a page"
                     />
                 </div>
+                <Popover>
+                    <PopoverTrigger className="w-full mt-4">
+                        <Item label="Trash" icon={Trash} />
+                    </PopoverTrigger>
+                    <PopoverContent
+                        className="p-0 w-72"
+                        side={isMobile ? "bottom" : "right"}
+                    >
+                        <TrashBox />
+                    </PopoverContent>
+                </Popover>
                 <div
                     onMouseDown={handleMouseDown}
                     onClick={resetWidth}
